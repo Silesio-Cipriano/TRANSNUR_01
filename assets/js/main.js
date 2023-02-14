bb9e934a51a15691f9927bae8b07318a47b61ca2
Silésio Leudério Cipriano <silesioleuderiocipriano@hotmail.com>
2023-02-14 17:42:43 +0200

    feat: added the galery

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,60 +103,61 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Porfolio isotope and filter
    */
-  let portfolionIsotope = document.querySelector('.portfolio-isotope');
-
-  if (portfolionIsotope) {
-    let portfolioFilter = portfolionIsotope.getAttribute(
-      'data-portfolio-filter'
-    )
-      ? portfolionIsotope.getAttribute('data-portfolio-filter')
-      : '*';
-    let portfolioLayout = portfolionIsotope.getAttribute(
-      'data-portfolio-layout'
-    )
-      ? portfolionIsotope.getAttribute('data-portfolio-layout')
-      : 'masonry';
-    let portfolioSort = portfolionIsotope.getAttribute('data-portfolio-sort')
-      ? portfolionIsotope.getAttribute('data-portfolio-sort')
-      : 'original-order';
-
-    window.addEventListener('load', () => {
-      let portfolioIsotope = new Isotope(
-        document.querySelector('.portfolio-container'),
-        {
-          itemSelector: '.portfolio-item',
-          layoutMode: portfolioLayout,
-          filter: portfolioFilter,
-          sortBy: portfolioSort,
-        }
-      );
-
-      let menuFilters = document.querySelectorAll(
-        '.portfolio-isotope .portfolio-flters li'
-      );
-      menuFilters.forEach(function (el) {
-        el.addEventListener(
-          'click',
-          function () {
-            document
-              .querySelector(
-                '.portfolio-isotope .portfolio-flters .filter-active'
-              )
-              .classList.remove('filter-active');
-            this.classList.add('filter-active');
-            portfolioIsotope.arrange({
-              filter: this.getAttribute('data-filter'),
-            });
-            if (typeof aos_init === 'function') {
-              aos_init();
-            }
-          },
-          false
-        );
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows',
       });
-    });
-  }
 
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on(
+        'click',
+        '#portfolio-flters li',
+        function (e) {
+          e.preventDefault();
+          portfolioFilters.forEach(function (el) {
+            el.classList.remove('filter-active');
+          });
+          this.classList.add('filter-active');
+
+          portfolioIsotope.arrange({
+            filter: this.getAttribute('data-filter'),
+          });
+          portfolioIsotope.on('arrangeComplete', function () {
+            AOS.refresh();
+          });
+        },
+        true
+      );
+    }
+  });
+
+  /**
+   * Initiate portfolio lightbox
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox',
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+  });
   /**
    * Init swiper slider with 1 slide at once in desktop view
    */
