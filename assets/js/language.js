5db52900d909d723c8b57e5fa7e6d999f8494322
Silésio Leudério Cipriano <silesioleuderiocipriano@hotmail.com>
2023-02-14 22:36:58 +0200

    fix

---
+++ assets/js/language.js
@@ -0,0 +1,231 @@
+var contentx = {
+  en: {
+    begin_opt: 'Home',
+    about_opt: 'About Us',
+    services_opt: 'Services',
+    contact_opt: 'Contact Us',
+    main_title: '<span>Transport</span> solution for your business',
+    down_left:
+      'We specialize in offering efficient transport solutions for cargo and goods.',
+    our_customers: 'Our Customers',
+    our_solution_txt:
+      'We offer Cargo and Goods Transport solutions observing and analyzing the particularity and ' +
+      'needs of each client, as we know that each client/business is unique and deserves exclusive dedication.',
+    about_txt1:
+      'It is a company dedicated to offering cargo transport solutions, observing and analyzing the parti' +
+      'cularity and needs of each client, our transport activities cover the road modal, acting in dedicated oper' +
+      'ations, distribution, spot loads, express loads throughout the national territory, in addition to of custo' +
+      'mized solutions guaranteeing your total satisfaction.',
+    about_txt2:
+      'We are a young company specializing in cargo transport, training and dedication to the segments,' +
+      ' with safety, quality and the guarantee of excellence in the services provided.',
+    about_txt3:
+      'We have a type of vehicle that will serve your operation in an agile way, with quality, and the ' +
+      'flexibility that your operation needs, always with the best cost-benefit ratio in the market.',
+    our_mission_tt: 'Our mission',
+    our_mission_txt:
+      'Establish a partnership relationship with our customers, offering services and solutions wi' +
+      'th high added value and exclusively focused on their business and needs.”',
+    service_1: 'Transfer transport',
+    service_1_txt:
+      'Your business will have the right partner to transfer raw materials to the factory as well as' +
+      ' finished products to your warehouse.',
+    why_tt: 'Why choose our services?',
+    why_st: 'Our differential',
+    why_txt_1:
+      'We support clients of all sizes at every stage of their development',
+    why_txt_2:
+      'Choosing our Transnur services means choosing an experienced company committed to customer satisf' +
+      'action. We offer customized and efficient transport and logistics solutions, ensuring the safety and inte' +
+      'grity of your cargo. In addition, we have a highly qualified team to ensure punctual delivery and real-ti' +
+      'me tracking of your shipment. By choosing Transnur, you can rest assured that your goods will be in good ' +
+      'hands.',
+    why_txt_3:
+      'Count on us to be your strategic transportation partner, we will certainly contribute to the success of y' +
+      'our business.',
+    points_tt_1: 'safe packing',
+    points_txt_1:
+      'You are responsible for protecting the product during transport and ensuring that it arrives at its de' +
+      'stination in perfect condition.',
+    points_tt_2: 'Shipping all over the country',
+    points_txt_2:
+      'Our transport and logistics network covers the entire national territory, allowing us to offer flexibl' +
+      'e and personalized shipping solutions according to the needs of each client.',
+    points_tt_3: 'Delivery on time',
+    points_txt_3:
+      'We understand the importance of ensuring your goods arrive at their destination in a timely manner, an' +
+      'd we work hard to provide timely and reliable delivery solutions.',
+    points_tt_4: 'Fastest shipping',
+    points_txt_4:
+      "At Transnur, we understand that speed is essential in any transport and logistics process. That's why " +
+      "we offer fast and efficient shipping solutions to meet our customers' needs.",
+    points_tt_5: 'Area of Activity',
+    points_txt_5: 'National coverage',
+    footer_txt_1: 'We are available to help your business succeed!',
+    contact_btn: 'Contact Us',
+    send_email: 'Send',
+    cancel_btn: 'Cancel',
+    subject: 'Subject',
+    name: 'Name',
+    message: 'Type a message',
+  },
+  pt: {
+    begin_opt: 'Início',
+    about_opt: 'Sobre nós',
+    services_opt: 'Serviços',
+    contact_opt: 'Contacte-nos',
+    main_title: 'Solução de <span>transporte</span> para o seu negócio',
+    down_left:
+      'Nos especializamos em oferecer soluções de transporte eficientes\n' +
+      '            para cargas e mercadorias.',
+    our_customers: 'Nossos Clientes',
+    our_solution_txt:
+      '            Oferecemos soluções de Transporte de cargas e mercadorias observando\n' +
+      '            e analisando a particularidade e a necessidade de cada cliente, pois\n' +
+      '            sabemos que cada cliente/negócio é único e merece dedicação\n' +
+      '            exclusiva.',
+    about_txt1:
+      '          É uma empresa voltada a oferecer soluções de Transportes de cargas\n' +
+      '          observando e analisando a particularidade e a necessidade de cada\n' +
+      '          cliente, nossas actividades de transportes abrangem o modal\n' +
+      '          rodoviário, actuando em operações dedicadas, distribuição, cargas\n' +
+      '          spot, cargas expressas em todo território nacional, além de soluções\n' +
+      '          personalizadas garantindo a sua total satisfação.',
+    about_txt2:
+      '              Somos uma empresa jovem especialistas em Transportes de cargas,\n' +
+      '              capacitação e dedicação aos segmentos, com segurança, qualidade e\n' +
+      '              a garantia de excelência dos serviços prestados.',
+    about_txt3:
+      '              Possuímos um tipo de veículo que atenderá a sua operação de forma\n' +
+      '              ágil, com qualidade, e a flexibilidade que sua operação necessita,\n' +
+      '              sempre com a melhor relação custo x benefício de mercado.',
+    our_mission_tt: 'Nossa missão',
+    our_mission_txt:
+      '              Estabelecer uma relação de parceria com nossos clientes,\n' +
+      '              oferecendo serviços e soluções de alto valor agregado e voltados\n' +
+      '              exclusivamente ao seu negócio e as suas necessidades.”',
+    service_1: 'Transporte de transferencia',
+    service_1_txt:
+      '              O seu negócio terá o parceiro certo para realizar as\n' +
+      '              transferências de matéria prima para a fabrica como também de\n' +
+      '              produtos acabados para o seu armazém.',
+    why_tt: 'Porque escolher os nossos serviços?',
+    why_st: 'O nosso diferencial',
+    why_txt_1:
+      '                Apoiamos clientes de todas as dimensões em todas as fases do seu\n' +
+      '                desenvolvimento',
+    why_txt_2:
+      '                Escolher os nossos serviços da Transnur significa optar por uma\n' +
+      '                empresa experiente e comprometida com a satisfação de seus\n' +
+      '                clientes. Oferecemos soluções de transporte e logística\n' +
+      '                personalizadas e eficientes, garantindo a segurança e a\n' +
+      '                integridade de suas cargas. Além disso, contamos com uma equipe\n' +
+      '                altamente capacitada para garantir a entrega pontual e o\n' +
+      '                acompanhamento em tempo real de seu envio. Ao escolher a\n' +
+      '                Transnur, você tem a tranquilidade de saber que suas mercadorias\n' +
+      '                estarão em boas mãos.',
+    why_txt_3:
+      '                Conte connosco para ser seu parceiro transportador estratégico,\n' +
+      '                certamente iremos contribuir para o sucesso do seu negócio.',
+    points_tt_1: 'Embalagem segura',
+    points_txt_1:
+      '                  É responsável por proteger o produto durante o transporte e\n' +
+      '                  garantir que ele chegue ao destino em perfeitas condições.',
+    points_tt_2: 'Envio em todo país',
+    points_txt_2:
+      '                  Nossa rede de transporte e logística abrange todo o território\n' +
+      '                  nacional, permitindo que possamos oferecer soluções de envio\n' +
+      '                  flexíveis e personalizadas de acordo com as necessidades de\n' +
+      '                  cada cliente.',
+    points_tt_3: 'Entrega dentro do prazo',
+    points_txt_3:
+      '                  Compreendemos a importância de garantir que suas mercadorias\n' +
+      '                  cheguem ao destino em tempo hábil, e trabalhamos arduamente\n' +
+      '                  para oferecer soluções de entrega pontual e confiável.',
+    points_tt_4: 'Envio mais rápido',
+    points_txt_4:
+      '                  Na Transnur, entendemos que a rapidez é essencial em qualquer\n' +
+      '                  processo de transporte e logística. Por isso, oferecemos\n' +
+      '                  soluções de envio rápido e eficiente para atender às\n' +
+      '                  necessidades de nossos clientes.',
+    points_tt_5: 'Área de Actuação',
+    points_txt_5: 'obertura nacional',
+    footer_txt_1: 'Estamos à disposição para ajudar no sucesso do seu negócio!',
+    contact_btn: 'Contactar',
+    send_email: 'Enviar',
+    cancel_btn: 'Cancelar',
+    subject: 'Assunto',
+    name: 'Nome',
+    message: 'Digite uma mensagem',
+  },
+};
+
+function updateContent(language) {
+  document.getElementById('begin_opt').innerHTML = contentx[language].begin_opt;
+  document.getElementById('about_opt').innerHTML = contentx[language].about_opt;
+  document.getElementById('services_opt').innerHTML =
+    contentx[language].services_opt;
+  document.getElementById('contact_opt').innerHTML =
+    contentx[language].contact_opt;
+  document.getElementById('main_title').innerHTML =
+    contentx[language].main_title;
+  document.getElementById('down_left').innerHTML = contentx[language].down_left;
+  document.getElementById('our_customers').innerHTML =
+    contentx[language].our_customers;
+  document.getElementById('our_solution_txt').innerHTML =
+    contentx[language].our_solution_txt;
+  document.getElementById('about_txt1').innerHTML =
+    contentx[language].about_txt1;
+  document.getElementById('about_txt2').innerHTML =
+    contentx[language].about_txt2;
+  document.getElementById('about_txt3').innerHTML =
+    contentx[language].about_txt3;
+  document.getElementById('our_mission_tt').innerHTML =
+    contentx[language].our_mission_tt;
+  document.getElementById('our_mission_txt').innerHTML =
+    contentx[language].our_mission_txt;
+  document.getElementById('service_1').innerHTML = contentx[language].service_1;
+  document.getElementById('service_1_txt').innerHTML =
+    contentx[language].service_1_txt;
+  document.getElementById('why_tt').innerHTML = contentx[language].why_tt;
+  document.getElementById('why_st').innerHTML = contentx[language].why_st;
+  document.getElementById('why_txt_1').innerHTML = contentx[language].why_txt_1;
+  document.getElementById('why_txt_2').innerHTML = contentx[language].why_txt_2;
+  document.getElementById('why_txt_3').innerHTML = contentx[language].why_txt_3;
+  document.getElementById('points_tt_1').innerHTML =
+    contentx[language].points_tt_1;
+  document.getElementById('points_txt_1').innerHTML =
+    contentx[language].points_txt_1;
+  document.getElementById('points_tt_2').innerHTML =
+    contentx[language].points_tt_2;
+  document.getElementById('points_txt_2').innerHTML =
+    contentx[language].points_txt_2;
+  document.getElementById('points_tt_3').innerHTML =
+    contentx[language].points_tt_3;
+  document.getElementById('points_txt_3').innerHTML =
+    contentx[language].points_txt_3;
+  document.getElementById('points_tt_4').innerHTML =
+    contentx[language].points_tt_4;
+  document.getElementById('points_txt_4').innerHTML =
+    contentx[language].points_txt_4;
+  document.getElementById('points_tt_5').innerHTML =
+    contentx[language].points_tt_5;
+  document.getElementById('points_txt_5').innerHTML =
+    contentx[language].points_txt_5;
+  document.getElementById('footer_txt_1').innerHTML =
+    contentx[language].footer_txt_1;
+  document.getElementById('contact_btn').innerHTML =
+    contentx[language].contact_btn;
+  document.getElementById('send_email').innerHTML =
+    contentx[language].send_email;
+  document.getElementById('cancel_btn').innerHTML =
+    contentx[language].cancel_btn;
+  document.getElementById('subject').placeholder = contentx[language].subject;
+  document.getElementById('name').placeholder = contentx[language].name;
+  document.getElementById('message').placeholder = contentx[language].message;
+  document.getElementById('contact_2').innerHTML =
+    contentx[language].contact_opt;
+  document.getElementById('services_2').innerHTML =
+    contentx[language].services_opt;
+  document.getElementById('about_2').innerHTML = contentx[language].about_opt;
+}
