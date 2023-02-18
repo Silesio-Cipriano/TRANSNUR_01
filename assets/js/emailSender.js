8883d362b233282b26b1f31bacddfe2a9bd3179c
Silésio Leudério Cipriano <silesioleuderiocipriano@hotmail.com>
2023-02-18 08:24:12 +0200

    fix: logo  cocaCola

--- assets/js/emailSender.js
+++ assets/js/emailSender.js
@@ -1,8 +1,20 @@
 function sendEmail() {
-    var nome = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var assunto = document.getElementById("subject").value;
-    var mensagem = document.getElementById("message").value;
-    var corpoEmail = "Nome: " + nome + "%0D%0A" + "E-mail: " + email + "%0D%0A" + "Assunto: " + assunto + "%0D%0A" + "Mensagem:%0D%0A" + mensagem;
-    window.location.href = "mailto:iqbal@transnur.co.mz?subject=" + assunto + "&body=" + corpoEmail;
+  var nome = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var assunto = document.getElementById('subject').value;
+  var mensagem = document.getElementById('message').value;
+  var corpoEmail =
+    'Nome: ' +
+    nome +
+    '%0D%0A' +
+    'E-mail: ' +
+    email +
+    '%0D%0A' +
+    'Assunto: ' +
+    assunto +
+    '%0D%0A' +
+    'Mensagem:%0D%0A' +
+    mensagem;
+  window.location.href =
+    'mailto:info@transnur.co.mz?subject=' + assunto + '&body=' + corpoEmail;
 }
